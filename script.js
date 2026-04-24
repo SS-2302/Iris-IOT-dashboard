740649a023132583595ca636eee1229e7fc4f193
Santhosh1023 <santhoshs0204@gmail.com>
2026-04-24 17:08:19 +0530

    update hamburger menu

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ const hamburger = document.getElementById("hamburger");
 const menu = document.querySelector(".cta-btn");
 const links = document.querySelectorAll(".nav-btn");
 
-// toggle menu
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
   hamburger.classList.toggle("active");
 });
 
-// close menu when any link is clicked
 links.forEach(link => {
   link.addEventListener("click", () => {
     menu.classList.remove("active");
